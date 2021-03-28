8dab098017230ffeb4e05cf5283fba36df0e98ca
edward <eduardcp20@gmail.com>
2021-03-27 21:40:30 -0400

    27/03/2021

---
+++ android/app/src/main/kotlin/institutoite/profesional_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package institutoite.profesional_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:profesional_app/AllScreens/loginScreen.dart';
+import 'package:profesional_app/AllScreens/registerationScreen.dart';
+import 'package:profesional_app/AllScreens/mainscreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+DatabaseReference usersRef = FirebaseDatabase.instance.reference().child("users");
+DatabaseReference driversRef = FirebaseDatabase.instance.reference().child("drivers");
+DatabaseReference rideRequestRef = FirebaseDatabase.instance.reference().child("Ride Requests");
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instituto Ite',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginScreen.idScreen,
+        routes:
+        {
+          RegisterationScreen.idScreen: (context) => RegisterationScreen(),
+          LoginScreen.idScreen: (context) => LoginScreen(),
+          MainScreen.idScreen: (context) => MainScreen(),
+        },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
